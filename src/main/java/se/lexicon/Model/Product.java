37c7e4f5b9ecba442d7982a46c38731abb9e355f
binleiden <daniel.petersson83@gmail.com>
2024-03-26 09:56:53 +0100

    Product declared by uml diagram

---
+++ src/main/java/se/lexicon/Model/Product.java
@@ -0,0 +1,51 @@
+package se.lexicon.Model;
+
+public class Product {
+    //Fields
+    private static int sequencer = 0;
+    private int id;
+    private double price;
+
+    private String productName;
+
+    //Constructors
+    public Product(double price, String productName) {
+        this.price = price;
+        this.productName = productName;
+        this.id= getNextId();
+
+    }
+    //Setters
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //Getters
+
+    private static int getNextId() {
+        return ++sequencer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    //Methods
+    public void setProductName(String productName) {
+        if (productName == null) throw new IllegalArgumentException("productName is null");
+        this.productName = productName;
+    }
+
+    public abstract String examine();
+
+    public abstract String use();
+}
