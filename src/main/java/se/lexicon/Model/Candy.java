fd29d93fbd320e903c4efb6adeb39395f5e4b414
binleiden <daniel.petersson83@gmail.com>
2024-03-28 09:07:24 +0100

    added id to examine

--- src/main/java/se/lexicon/Model/Candy.java
+++ src/main/java/se/lexicon/Model/Candy.java
@@ -29,7 +29,7 @@ public class Candy extends Product {
     //Methods
     @Override
     public String examine() {
-        return "Product" + "\n" +"Name: " + getProductName() + "\n" + "Type: " + getType() + "\n" + "Taste: " + getTaste() + "\n" + "Price: " + getPrice()+ "\n";
+        return "Product" + "\n" +"ID: " + getId() + "\n" + "\n" +"Name: " + getProductName() + "\n" + "Type: " + getType() + "\n" + "Taste: " + getTaste() + "\n" + "Price: " + getPrice()+ "\n";
     }
 
     @Override
--- src/main/java/se/lexicon/Model/Snack.java
+++ src/main/java/se/lexicon/Model/Snack.java
@@ -20,7 +20,7 @@ public class Snack extends Product {
     //Methods
     @Override
     public String examine() {
-        return "Product" + "\n" +"Name: " + getProductName() + "\n" + "Type: " + type + "\n" + "Taste: " + taste + "\n" + "Price: " + getPrice()+"\n";
+        return "Product" + "\n" +"ID: " + getId() + "\n" +"Name: " + getProductName() + "\n" + "Type: " + type + "\n" + "Taste: " + taste + "\n" + "Price: " + getPrice()+"\n";
     }
 
     @Override
